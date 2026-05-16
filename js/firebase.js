ec6e6b98b11cf57b51d5c81a830302ebd6b76c92
rajesh-10-hash <rajeshtenkayala6302@gmail.com>
2026-05-16 16:17:17 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+/* ═══════════════════════════════════
+   RC CENTER — FIREBASE CONFIG
+   ═══════════════════════════════════ */
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyCclOlFqdZ80pOY070Z8dTNU-KXxG4G_do",
+  authDomain:        "rc-center-98e47.firebaseapp.com",
+  projectId:         "rc-center-98e47",
+  storageBucket:     "rc-center-98e47.firebasestorage.app",
+  messagingSenderId: "849354228554",
+  appId:             "1:849354228554:web:9b01a7d7bc78151ab7f7b3",
+  measurementId:     "G-CWVBRDS1DZ"
+};
+
+firebase.initializeApp(firebaseConfig);
